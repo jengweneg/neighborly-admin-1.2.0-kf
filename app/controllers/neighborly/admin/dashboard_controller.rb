215548dbc83d76fe8575217ba74f517cd8122025
Julie <jdaprato07@gmail.com>
2021-03-23 19:19:16 +0100

    Add statistics

--- app/controllers/neighborly/admin/dashboard_controller.rb
+++ app/controllers/neighborly/admin/dashboard_controller.rb
@@ -3,6 +3,10 @@ module Neighborly::Admin
     inherit_resources
     defaults  resource_class: Statistics
     actions :index
+
+    def index
+      @statistics = NeighborlyAdminStatistics.all
+    end
   end
 end
 
