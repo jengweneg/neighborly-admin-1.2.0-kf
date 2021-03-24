892903ae9d32eaff3b6a2348a3a391dc4572711a
Julie <jdaprato07@gmail.com>
2021-03-24 08:44:19 +0100

    Fix error sql not valid

--- app/controllers/neighborly/admin/financials_controller.rb
+++ app/controllers/neighborly/admin/financials_controller.rb
@@ -23,7 +23,7 @@ module Neighborly::Admin
 
     protected
     def projects
-      apply_scopes(Project).includes(:user).order("CASE state WHEN 'successful' THEN 1 WHEN 'waiting_funds' THEN 2 ELSE 3 END, (projects.expires_at)::date DESc")
+      apply_scopes(Project).includes(:user).order(Arel.sql("CASE state WHEN 'successful' THEN 1 WHEN 'waiting_funds' THEN 2 ELSE 3 END, (projects.expires_at)::date DESc"))
     end
 
     def collection
