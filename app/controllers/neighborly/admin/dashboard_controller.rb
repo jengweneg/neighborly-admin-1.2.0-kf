f62a8adae3c1331416154a5a197b7c1f21bf697c
Julie <jdaprato07@gmail.com>
2021-03-30 09:16:11 +0200

    Fix statistics default 0

--- app/controllers/neighborly/admin/dashboard_controller.rb
+++ app/controllers/neighborly/admin/dashboard_controller.rb
@@ -5,7 +5,23 @@ module Neighborly::Admin
     actions :index
 
     def index
-      @statistics = collection.first || Statistics.new
+      @statistics = collection.first || Statistics.new(
+        total_users: 0,
+        total_organization_users:0,
+        total_personal_users: 0,
+        total_channel_users: 0,
+        total_communities: 0,
+        total_contributions: 0,
+        total_contributors: 0,
+        total_contributed: 0,
+        total_contributed: 0,
+        total_projects: 0,
+        total_projects: 0,
+        total_projects_success: 0,
+        total_projects_online: 0,
+        total_projects_draft: 0,
+        total_projects_soon: 0,
+        )
     end
   end
 end
